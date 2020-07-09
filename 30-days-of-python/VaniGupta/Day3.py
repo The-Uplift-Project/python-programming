9a4fdc093b7a0612df71d3d7c9d3b29a6a5833bf
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-10 01:30:07 +0530

    Create Day3.py

---
+++ 30-days-of-python/VaniGupta/Day3.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    if n%2 == 1 or (n%2==0 and n<=20 and n>=6): print("Weird")
+    else: print("Not Weird")
