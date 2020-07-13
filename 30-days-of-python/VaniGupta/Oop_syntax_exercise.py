f9f07b290660443d7ce3f06ecb985ff05f9695ec
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-14 01:50:43 +0530

    Added assert statements
    Assert statements added to check the correctness of code

--- 30-days-of-python/VaniGupta/Oop_syntax_exercise.py
+++ 30-days-of-python/VaniGupta/Oop_syntax_exercise.py
@@ -56,3 +56,31 @@ class SalesPerson():
 
     def calculate_commission(self,val):
         return val*self.total_sales
+
+def check_results():
+    pants_one = Pants('red', 35, 36, 15.12)
+    pants_two = Pants('blue', 40, 38, 24.12)
+    pants_three = Pants('tan', 28, 30, 8.12)
+    
+    salesperson = SalesPerson('Amy', 'Gonzalez', 2581923, 40000)
+    
+    assert salesperson.first_name == 'Amy'
+    assert salesperson.last_name == 'Gonzalez'
+    assert salesperson.employee_id == 2581923
+    assert salesperson.salary == 40000
+    assert salesperson.pants_sold == []
+    assert salesperson.total_sales == 0
+    
+    salesperson.sell_pants(pants_one)
+    salesperson.pants_sold[0] == pants_one.color
+    
+    salesperson.sell_pants(pants_two)
+    salesperson.sell_pants(pants_three)
+    
+    assert len(salesperson.pants_sold) == 3
+    assert round(salesperson.calculate_sales(),2) == 47.36
+    assert round(salesperson.calculate_commission(.1),2) == 4.74
+    
+    print('Great job, you made it to the end of the code checks!')
+    
+check_results()
