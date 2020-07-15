0db36b15c1c4233ad77825c8cbf5aa6c2afbb124
Saloni <saloniswagata@gmail.com>
2020-07-15 21:57:15 +0530

    corrected errors for caesar cipher encryption

--- 30-days-of-python/Saloni Swagata 30DOC/Caesar Cipher Encryption.py
+++ 30-days-of-python/Saloni Swagata 30DOC/Caesar Cipher Encryption.py
@@ -1,18 +1,8 @@
-s = input("Enter the string to be encrypted: ").split()
-d=""
-if len(s)>1:
-    for word in s:
-        word.upper()
-        for letter in word:
-            k = ord(letter)-65
-            n = (k+3)%26
-            d+=chr(n)
-        d+=" "
-else:
-    S=s[0]
-    S.upper()
-    for letter in S:
-        k = ord(letter)-65
-        n = (k+3)%26
-        d+=chr(n)
-print(f"Encryted String: {d}")
+s = input("Enter the string to be encrypted: ")
+e=""
+for letter in s:
+    if letter.isupper():
+         e+= chr((ord(letter) + 3-65) % 26 + 65)
+    else:
+        e += chr((ord(letter) + 3 - 97) % 26 + 97)
+print(f"Encryted String: {e}")
