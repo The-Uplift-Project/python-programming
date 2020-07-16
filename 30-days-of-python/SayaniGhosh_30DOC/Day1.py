b089ded4774329953987a101b25f55ebaf93c16f
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-07-16 22:05:00 +0530

    Day1

---
+++ 30-days-of-python/SayaniGhosh_30DOC/Day1.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+z=""
+# Read and save an integer, double, and String to your variables.
+x=int(input("Enter integer "))
+y=float(input("Enter double "))
+z=input("Enter string ")
+# Print the sum of both integer variables on a new line.
+print(x+i)
+# Print the sum of the double variables on a new line.
+print(y+d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+z)
