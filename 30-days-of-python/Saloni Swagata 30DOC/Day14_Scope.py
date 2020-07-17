840f89eeccefe8d39c66da50c7a2414ee506cb0e
Saloni <saloniswagata@gmail.com>
2020-07-17 13:08:18 +0530

    Added Day 14 Hackerrank code

---
+++ 30-days-of-python/Saloni Swagata 30DOC/Day14_Scope.py
@@ -0,0 +1,18 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    def computeDifference(self):
+        self.__elements.sort()
+        self.maximumDifference= (self.__elements[-1]-self.__elements[0])
+	# Add your code here
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
