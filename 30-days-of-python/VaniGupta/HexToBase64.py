2c0eaacba3e54f45030af8132dee2343b2032545
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-18 23:14:32 +0530

    Create HexToBase64.py

---
+++ 30-days-of-python/VaniGupta/HexToBase64.py
@@ -0,0 +1,8 @@
+# The string:
+# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# the above string is given in bytes
+
+
+# Should produce:
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
