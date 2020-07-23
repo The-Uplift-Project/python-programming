9c1abb0f53ebbe0297fc7f81af23f71f37928832
Ujjwal <37502171+BellatrixLestrangee@users.noreply.github.com>
2020-07-23 23:00:49 +0530

    Create Day_15.py

---
+++ 30-days-of-python/Ujjwal/Day_15.py
@@ -0,0 +1,10 @@
+# Day 15 Hackerrank Challenge
+
+def insert(self,head,data): 
+    if head is None:
+        head = Node(data)
+    elif head.next is None:
+        head.next = Node(data)
+    else: 
+        self.insert(head.next, data)
+    return head
