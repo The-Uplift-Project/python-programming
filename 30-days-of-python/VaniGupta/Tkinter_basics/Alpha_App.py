4fe0d7c002a4219e8a63bf3192825f5355bac405
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-26 13:38:01 +0530

    Update Alpha_App.py

--- 30-days-of-python/VaniGupta/Tkinter_basics/Alpha_App.py
+++ 30-days-of-python/VaniGupta/Tkinter_basics/Alpha_App.py
@@ -43,3 +43,24 @@ display = Entry(frame1, text = str1, font = ('chiller', 44, 'bold'),\
 	bg = 'light yellow', fg = 'black', bd = 14, justify = CENTER, width = 36)
 display.grid(row = 0, column = 0, columnspan = 7)
 
+#===========================Row 1===============================#
+
+btnA = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'A', bg = 'light cyan', command = lambda : alpha("A"))
+btnB = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'B', bg = 'light cyan', command = lambda : alpha("B"))
+btnC = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'C', bg = 'light cyan', command = lambda : alpha("C"))
+btnD = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'D', bg = 'light cyan', command = lambda : alpha("D"))
+btnE = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'E', bg = 'light cyan', command = lambda : alpha("E"))
+btnF = Button(frame1, bd = 4, font = ('chiller', 20, 'bold'), width = 12,\
+	height = 3, text = 'F', bg = 'light cyan', command = lambda : alpha("F"))
+
+btnA.grid(row = 1, column = 0)
+btnB.grid(row = 1, column = 1)
+btnC.grid(row = 1, column = 2)
+btnD.grid(row = 1, column = 4)
+btnE.grid(row = 1, column = 5)
+btnF.grid(row = 1, column = 6)
