1445e9fb93548187b89c0b5724bd49dc7ab9260a
Vani Gupta <53790649+vanigupta20024@users.noreply.github.com>
2020-07-26 13:36:31 +0530

    Update Alpha_App.py

--- 30-days-of-python/VaniGupta/Tkinter_basics/Alpha_App.py
+++ 30-days-of-python/VaniGupta/Tkinter_basics/Alpha_App.py
@@ -13,3 +13,13 @@ root.geometry('1038x587+100+100')
 
 root.config(background = 'white')
 str1 = StringVar()
+
+str1.set('Alphabet application for kids!')
+frame1 = Frame(root, bg = 'white')
+frame1.grid()
+
+disp = Canvas(frame1, width = 135, height = 120)
+disp.grid(row = 1, column = 3)
+img = ImageTk.PhotoImage(Image.open("pic.jpg"))
+image = disp.create_image(80, 160, image = img)
+
