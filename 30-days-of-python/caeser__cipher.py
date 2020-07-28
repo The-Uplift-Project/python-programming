19ae10543db701a2a1ba342415e4b7016846896e
Vijeta Saini <60733674+vijetasaini0211@users.noreply.github.com>
2020-07-28 15:27:43 +0530

    Update caeser__cipher.py

--- 30-days-of-python/caeser__cipher.py
+++ 30-days-of-python/caeser__cipher.py
@@ -10,7 +10,7 @@ for i in range(n):
     character=string_input[i]
     location=alphabets.find(character)
     new_loc=(location+key)%26
-    print("character",character,"location",location,"new location",new_loc)
+    print("character",character," location",location," new location",new_loc)
     soutpt+=alphabets[new_loc]
 
-print(otpt)
+print(soutpt)
