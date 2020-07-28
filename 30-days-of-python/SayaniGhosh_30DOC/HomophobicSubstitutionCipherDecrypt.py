761aea45d7ed74160e139f2c926a7b76e0eb812b
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-07-28 15:42:14 +0530

    Added Homophobic Substitution Cipher Decrypt Code

---
+++ 30-days-of-python/SayaniGhosh_30DOC/HomophobicSubstitutionCipherDecrypt.py
@@ -0,0 +1,41 @@
+# Decrypt Homophobic Substitution Cipher Messages
+
+def hs_decrypt(strn, k):
+    msg = ""
+    for ch in strn:
+        for i in range(len(k)):
+            lis = list(k[i])
+            if ch in lis:
+                msg += chars[i]
+    return msg
+
+
+def key():
+    t = list(input("Enter space seperated key for ' ' ',' '.' and 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : "))
+
+    li = []
+    s = ""
+    for i in range(len(t)):
+        if t[i] == ' ':
+            li.append(s)
+            s = ""
+            continue
+        s += t[i]
+        if i == len(t) - 1:
+            li.append(s)
+    return li
+
+
+t = ""
+while t != "exit":
+    chars = " ,.ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    print("Enter message to decrypt")
+    st = input()
+    k = key()
+
+    e_msg = hs_decrypt(st, k)
+    print(e_msg)
+
+    print("Want to encrypt again? Press 'c'. Or Press 'exit' to end process.")
+    t = input()
