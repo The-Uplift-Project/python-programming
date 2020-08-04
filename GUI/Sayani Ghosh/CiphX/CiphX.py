3e8008bfcf2d514d138d231953aea14b695e24d3
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-08-04 11:32:23 +0530

    Updated CiphX - Added Autokey Cipher

--- GUI/Sayani Ghosh/CiphX/CiphX.py
+++ GUI/Sayani Ghosh/CiphX/CiphX.py
@@ -1,4 +1,5 @@
 #CIPHER APP
+
 from tkinter import *
 from PIL import ImageTk, Image
 import random
@@ -359,6 +360,107 @@ def sel():
         btn4.configure(font=("Helvetica", 10))
         btn4.place(x=800, y=600, anchor="center")
 
+    if s == "Autokey Cipher":
+        # Autokey cipher
+
+        et = Toplevel()
+        et.title("Autokey Cipher")
+        et.iconbitmap('disneyland.ico')
+        et.configure(bg="Pink")
+        et.geometry("2000x2000")
+        et.bind('<Home>', lambda event: et.state('normal'))
+        et.bind('<F12>', lambda event: et.state('zoomed'))
+
+
+        def Encode():
+            txt = ent1.get()
+            k = ent2.get()
+
+            ls = k
+            i = 0
+            while len(ls) != len(txt):
+                ls += txt[i]
+                i += 1
+
+            st = ""
+            for i in range(len(txt)):
+                s = ((ord(txt[i]) + ord(ls[i])) % 26) + ord('A')
+                st += chr(s)
+
+
+            global lbl
+            lbl = Label(et, text="Ciphered Message : "+st, font=("Helvetica", 25))
+            lbl.place(x=700, y=500, anchor="center")
+
+        def Decode():
+            txt = ent1.get()
+            k = ent2.get()
+
+            ls = k
+            i = 0
+            while len(ls) != len(txt):
+                ls += chr(((ord(txt[i]) - ord(ls[i])) % 26) + ord('A'))
+                i += 1
+
+            st = ""
+            for i in range(len(txt)):
+                s = ((ord(txt[i]) - ord(ls[i]) + 26) % 26) + ord('A')
+                st += chr(s)
+
+            global lbl
+            lbl = Label(et, text="Original Message : "+st, font=("Helvetica", 25))
+            lbl.place(x=700, y=500, anchor="center")
+
+        def Ref():
+            ent1.delete(0, END)
+            ent2.delete(0, END)
+            lbl.destroy()
+
+        def Back():
+            et.destroy()
+
+        lb = Label(et, padx=30, text="VIGENERE CIPHER", font=("Helvetica", 40))
+        lb.place(x=700, y=50, anchor="center")
+
+        k1 = Label(et, padx=10, text="Press F12 : FullScreen", bg="Lightyellow", font=("Helvetica", 10))
+        k1.place(x=600, y=100, anchor="center")
+
+        k2 = Label(et, padx=10, text="Press Home : NormalScreen", bg="Lightyellow", font=("Helvetica", 10))
+        k2.place(x=800, y=100, anchor="center")
+
+        lb1 = Label(et, padx=30, text="Enter Message :", font=("Helvetica", 20))
+        lb1.place(x=700, y=150, anchor="center")
+
+        ent1 = Entry(et, width=20, background="Skyblue", borderwidth=3)
+        ent1.configure(font=("Helvetica", 20))
+        ent1.place(x=700, y=200, anchor="center")
+
+        lb2 = Label(et, padx=30, text="Enter KEY Value :", font=("Helvetica", 20))
+        lb2.place(x=700, y=250, anchor="center")
+
+        ent2 = Entry(et, width=20, background="Skyblue", borderwidth=3)
+        ent2.configure(font=("Helvetica", 20))
+        ent2.place(x=700, y=300, anchor="center")
+
+        # Create Button
+
+        btn1 = Button(et, borderwidth=0, padx=20, pady=20, command=Encode)
+        btn1.configure(font=("Helvetica", 20), image=im1, bg="Pink")
+        btn1.place(x=500, y=400, anchor="center")
+
+        btn2 = Button(et, borderwidth=0, padx=20, pady=20, command=Decode)
+        btn2.configure(font=("Helvetica", 20), image=im2, bg="Pink")
+        btn2.place(x=900, y=400, anchor="center")
+
+        btn3 = Button(et, text="REFRESH", borderwidth=0, padx=50, pady=10, command=Ref)
+        btn3.configure(font=("Helvetica", 10))
+        btn3.place(x=600, y=600, anchor="center")
+
+        btn4 = Button(et, text="BACK TO HOMESCREEN", borderwidth=0, padx=10, pady=10, command=Back)
+        btn4.configure(font=("Helvetica", 10))
+        btn4.place(x=800, y=600, anchor="center")
+
+
 def Close():
     root.destroy()
 
@@ -368,7 +470,7 @@ clicked.set("Caesar Cipher")
 lb = Label(root, padx=10, text="Choose The Ciphering Method :", font=("Helvetica", 25), background="Pink")
 lb.place(x=480, y=300, anchor="center")
 
-dd = OptionMenu(root, clicked, "Caesar Cipher", "Homophonic Substitution Cipher", "Vigenere Cipher")
+dd = OptionMenu(root, clicked, "Caesar Cipher", "Homophonic Substitution Cipher", "Vigenere Cipher", "Autokey Cipher")
 dd.configure(font=("Helvetica", 25), bg="Skyblue", width=27)
 dd.place(x=1000, y=300, anchor="center")
 
